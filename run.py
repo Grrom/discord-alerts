0e83d6bb953d4cb3cf3a38dd86c82581ba367606
Grrom <josejeromelalunio@gmail.com>
2024-07-14 08:42:46 +0000

    feat: add the test id and token to the env

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import base64
 import json
+import os
 
+from dotenv import load_dotenv
 from discord_alerts import discord_alert
 
+load_dotenv()
+
 test_message = {
-    "webhook-id": "1234567890",
-    "webhook-token": "abcdefg",
+    "webhook-id": os.environ.get("TEST_WEBHOOK_ID"),
+    "webhook-token": os.environ.get("TEST_WEBHOOK_TOKEN"),
     "content": "Hello, world!",
     "embeds": [
         {
@@ -23,7 +27,7 @@ test_message = {
         }
     ]
 }
-# when using this test message in pubsub, make sure you update True to true, as True is only for python
+# when using this test message as a json, make sure you update True to true, as True is only for python
 
 base64_message = base64.b64encode(json.dumps(test_message).encode("utf-8"))
 discord_alert({"data": base64_message}, None)
